74c8c690ea1ee8ff921fab9369bff730d1a64389
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2019-08-12 23:53:08 -0300

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'video-descritivo';
+  vid: HTMLVideoElement;
+
+  ngOnInit() {
+    this.vid = document.getElementById('myVideo').parentElement as HTMLVideoElement;
+  }
+
+  getCurTime() {
+    alert(this.vid.currentTime);
+  }
+
+  setCurTime() {
+    this.vid.currentTime = 5;
+  }
 }
